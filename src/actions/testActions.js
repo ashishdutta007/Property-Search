d32a9d2d7e29cd333c2acfed3fe5084cb6b62dd5
Ashish <ashish.dutta@carcility.com>
2019-09-08 23:11:50 +0530

    React-Redux initial boilerplate commit

--- src/actions/testActions.js
+++ src/actions/testActions.js
@@ -1,8 +1,13 @@
-import { NEW_ACTION_TYPE } from './actionTypes'
+import { NEW_ACTION_TYPE } from './actionTypes';
 
 // dispatch can be thought of as a resolver in case of async actions
 // we are able to capture the action here because of thunk
+// action creators ; generates an action object/function(redux-thunk)
+
 export function randomTestAction(actionPayload = {}) {
+  // redux-thunk allows us to return a function from action creators
+  // once this action creator returns a func/obj, which flows into the
+  //thunk middleware, if typeOf "Function", function gets executed
   return (dispatch) => {
     const payload = {
       type: 'NEW_ACTION_TYPE',
