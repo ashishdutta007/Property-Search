7086b0e49be9bcf3aaa82ab06513a343ff1e3524
Ashish <ashish.dutta@carcility.com>
2019-09-14 14:31:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,22 @@ import { Provider } from 'react-redux';
 import logo from './assets/logo.svg';
 import './App.css';
 import store from './store/store';
+import ListingPage from './containers/ListingPageContainer';
+import PropertyFilters from './containers/PropertyFilters';
 
+import 'rc-slider/assets/index.css';
 
 // Provider is react HOC used to pass redux store to the APP
-
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
+          <PropertyFilters type="rent" min={0} max={50000} />
+          <PropertyFilters type="area" min={0} max={5000} />
         </header>
+        <ListingPage />
       </div>
     </Provider>
   );
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import testReducer from './testReducer';
-
-// combineReducers combine all modular reducers into a rootReducer 
-// for the store to consume
-export default combineReducers({
-  testData: testReducer,
-});
